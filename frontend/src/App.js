2f2d35d6a51887eb20f87d65cf3ee3a3318d7232
Matthew Aitken <matt@itken.net>
2025-08-11 16:47:25 +0100

    Conditionally render elements depending on token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,12 +15,16 @@ import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, { action as authAction } from './pages/Authentication';
 import { action as logoutAction } from './pages/Logout';
+import { tokenLoader } from './util/auth'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: 'root',
+    // this loader gets the token from localstorage for all routes to access
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
