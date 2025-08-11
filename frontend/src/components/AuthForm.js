c874445e8b83c401eb69e8b82e68f9584874e495
Matthew Aitken <matt@itken.net>
2025-08-11 16:14:54 +0100

    Finish creating new user

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,4 +1,4 @@
-import { Form, Link, useSearchParams } from 'react-router-dom';
+import { Form, Link, useSearchParams, useActionData, useNavigation } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
@@ -7,10 +7,22 @@ function AuthForm() {
   const [ searchParams ] = useSearchParams();
   const isLogin = searchParams.get('mode') === 'login';
 
+  const data = useActionData();
+
+  // using useNavigation to get the status of things
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        {data && data.errors && <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>}
+        {data && data.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -23,7 +35,9 @@ function AuthForm() {
           <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
             {isLogin ? 'Create new user' : 'Login'}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? 'Submitting...' : 'Save'}
+          </button>
         </div>
       </Form>
     </>
